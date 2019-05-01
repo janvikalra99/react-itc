0fa685d6e29884e77fd72f9064642ce0f53c48b2
Janvi Kalra <janvi.kalra.21@dartmouth.edu>
2019-05-01 17:29:17 -0400

    added dashboard page

---
+++ src/components/questionsPage.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import '../style.scss';
+import * as Survey from 'survey-react';
+import Question from './question';
+import 'survey-react/survey.css';
+
+
+export default class QuestionsPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      id: 1,
+      questions: [1],
+      inPreview: false,
+      surveyjs: null,
+    };
+  }
+
+  deleteQuestion = (id) => {
+    // find want index value the ID is at
+    let index = 0;
+    while (this.state.questions[index] !== id) {
+      index += 1;
+    }
+    // make a separate copy of the array
+    const array = [...this.state.questions];
+    // delete that element from the list using it's index location
+    if (array.length > 1) {
+      array.splice(index, 1);
+    }
+    this.setState({ questions: array });
+  }
+
+  addQuestion = () => {
+    this.setState(prevState => ({ id: prevState.id + 1 }));
+
+    this.setState((prevState) => {
+      const array = prevState.questions;
+      array.push(prevState.id);
+      return { questions: array };
+    });
+  }
+
+  startPreview = () => {
+    this.setState({
+      inPreview: true,
+    });
+  }
+
+  endPreview = () => {
+    this.setState({
+      inPreview: false,
+    });
+  }
+
+  render() {
+    if (this.state.inPreview) {
+      return (
+        <div>
+          <script src="https://surveyjs.azureedge.net/1.0.79/survey.react.min.js" />
+          <link href="https://surveyjs.azureedge.net/1.0.79/survey.css" type="text/css" rel="stylesheet" />
+          <button type="button" onClick={this.endPreview}> End Preview </button>
+          <Survey.Survey json={this.state.surveyjs} />
+        </div>
+      );
+    }
+    return (
+      <div>
+        {this.state.questions.map(x => (<Question id={x} addQuestion={this.addQuestion} deleteQuestion={this.deleteQuestion} key={x} />))}
+        <button type="button" onClick={this.addQuestion}> Add Question </button>
+        <button type="button" onClick={this.startPreview}> Start Preview </button>
+      </div>
+    );
+  }
+}
+
+
+// <button type="button" onClick={this.handleClick}>delete</button>
--- src/index.js
+++ src/index.js
@@ -1,48 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './style.scss';
-import Question from './components/question';
+import QuestionsPage from './components/questionsPage';
 
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      id: 1,
-      questions: [1],
+      onQuestionsPage: false,
     };
   }
 
-  deleteQuestion = (id) => {
-    // find want index value the ID is at
-    let index = 0;
-    while (this.state.questions[index] !== id) {
-      index += 1;
-    }
-    // make a separate copy of the array
-    const array = [...this.state.questions];
-    // delete that element from the list using it's index location
-    if (array.length > 1) {
-      array.splice(index, 1);
-    }
-    this.setState({ questions: array });
-  }
-
-  addQuestion = () => {
-    this.setState(prevState => ({ id: prevState.id + 1 }));
-
-    this.setState((prevState) => {
-      const array = prevState.questions;
-      array.push(prevState.id);
-      return { questions: array };
+  goToQuestionsPage = () => {
+    this.setState({
+      onQuestionsPage: true,
     });
   }
 
+
   render() {
+    if (this.state.onQuestionsPage) {
+      return (
+        <div>
+          <QuestionsPage />
+        </div>
+      );
+    }
     return (
       <div>
-        {this.state.questions.map(x => (<Question id={x} addQuestion={this.addQuestion} deleteQuestion={this.deleteQuestion} key={x} />))}
-        <button type="button" onClick={this.addQuestion}> Add Question </button>
+        <p> dashboard </p>
+        <button type="button" onClick={this.goToQuestionsPage}> Create Survey </button>
       </div>
     );
   }

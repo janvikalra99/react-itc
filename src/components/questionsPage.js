bd32f801dbe66223a9e4b71e25b5ba003f66c962
Janvi Kalra <janvi.kalra.21@dartmouth.edu>
2019-05-02 02:51:45 -0400

    initial, dirty state

--- src/components/questionsPage.js
+++ src/components/questionsPage.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Map } from 'immutable';
 import '../style.scss';
 import * as Survey from 'survey-react';
 import Question from './question';
@@ -8,45 +7,32 @@ import 'survey-react/survey.css';
 export default class QuestionsPage extends React.Component {
   constructor(props) {
     super(props);
+    // create a question object
+    const questionType = {
+      type: 'multiplechoice',
+      name: 'id',
+      title: 'questionTitle',
+      isRequired: true,
+      choices: [
+        'choice1',
+        'choice2',
+      ],
+    };
+
     this.state = {
-      id: 1,
       questions: [1],
       inPreview: false,
       surveyjs: null,
-      questionMap: new Map(),
-      id: 0,
+      questionType,
+      questionMap: new Map([[1, questionType]]),
+      id: 1,
     };
-    this.createStaticSurvey = this.createStaticSurvey.bind(this);
+    this.publishSurvey = this.publishSurvey.bind(this);
   }
 
   onComplete = (survey, options) => {
   // Write survey results into database
     console.log(`Survey results: ${JSON.stringify(survey.data)}`);
-  }
-
-  deleteQuestion = (id) => {
-    // find want index value the ID is at
-    let index = 0;
-    while (this.state.questions[index] !== id) {
-      index += 1;
-    }
-    // make a separate copy of the array
-    const array = [...this.state.questions];
-    // delete that element from the list using it's index location
-    if (array.length > 1) {
-      array.splice(index, 1);
-    }
-    this.setState({ questions: array });
-  }
-
-  addQuestion = () => {
-    this.setState(prevState => ({ id: prevState.id + 1 }));
-
-    this.setState((prevState) => {
-      const array = prevState.questions;
-      array.push(prevState.id);
-      return { questions: array };
-    });
   }
 
   startPreview = () => {
@@ -61,10 +47,49 @@ export default class QuestionsPage extends React.Component {
     });
   }
 
-  createStaticSurvey() {
-    const title = 'default Title';
+  deleteQuestion = (id) => {
+    /* update the questions list */
+    // find want index value the ID is at
+    let index = 0;
+    while (this.state.questions[index] !== id) {
+      index += 1;
+    }
+    // make a separate copy of the array
+    const array = [...this.state.questions];
+    // delete that element from the list using it's index location
+    if (array.length > 1) {
+      array.splice(index, 1);
+    }
+    this.setState({ questions: array });
+
+    /* update the questions Map */
+    this.setState(prevState => ({
+      questionMap: prevState.questionMap.delete(id),
+    }));
+  }
+
+  addQuestion = () => {
+    // increment ID
+    this.setState(prevState => ({ id: prevState.id + 1 }));
+
+    /* update the questions list */
+    this.setState((prevState) => {
+      const array = prevState.questions;
+      array.push(prevState.id);
+      return { questions: array };
+    });
+
+    /* update the questions list */
+    const newQuestion = this.questionType;
+    this.setState(prevState => ({
+      questionMap: prevState.questionMap.set(prevState.id, newQuestion),
+    }));
+  }
+
+
+  publishSurvey() {
     const surveyData = {
-      title,
+      title: 'Default Title',
       pages: [
         {
           name: 'page1',
@@ -72,27 +97,61 @@ export default class QuestionsPage extends React.Component {
         },
       ],
     };
-
-    const newQuestion = {
-      type: 'radiogroup',
-      name: 'car',
-      title: 'What car are you driving?',
-      isRequired: true,
-      colCount: 4,
-      choices: [
-        'None',
-        'Ford',
-        'Vauxhall',
-        'Volkswagen',
-        'Nissan',
-      ],
-    };
-
+    const
     surveyData.pages[0].questions.push(newQuestion);
 
     console.log(`survey data: ${surveyData}`);
 
     this.setState({ surveyjs: surveyData });
+
+
+    // const newQuestion = {
+    //   type: 'radiogroup',
+    //   name: 'car',
+    //   title: 'What car are you driving?',
+    //   isRequired: true,
+    //   colCount: 4,
+    //   choices: [
+    //     'None',
+    //     'Ford',
+    //     'Vauxhall',
+    //     'Volkswagen',
+    //     'Nissan',
+    //   ],
+    // };
+
+
+    // const title = 'default Title';
+    // const surveyData = {
+    //   title,
+    //   pages: [
+    //     {
+    //       name: 'page1',
+    //       questions: [],
+    //     },
+    //   ],
+    // };
+    //
+    // const newQuestion = {
+    //   type: 'radiogroup',
+    //   name: 'car',
+    //   title: 'What car are you driving?',
+    //   isRequired: true,
+    //   colCount: 4,
+    //   choices: [
+    //     'None',
+    //     'Ford',
+    //     'Vauxhall',
+    //     'Volkswagen',
+    //     'Nissan',
+    //   ],
+    // };
+    //
+    // surveyData.pages[0].questions.push(newQuestion);
+    //
+    // console.log(`survey data: ${surveyData}`);
+    //
+    // this.setState({ surveyjs: surveyData });
   }
 
 
